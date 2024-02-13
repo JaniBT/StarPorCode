98945aee4e617540f23ce472f736ce99a4054b21
JaniBT <janoscsordas072@gmail.com>
2024-02-13 20:53:21 +0100

    testing if everything works well

--- starporcode/src/App.jsx
+++ starporcode/src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+
+function NavBar() {
+    return(
+        <div className="text-xl p-2 flex justify-center">
+            <a className="navibar" href="#">Kezdőlap</a>
+            <a className="navibar" href="#">Oldalak</a>
+            <a className="navibar" href="#">Tanulás</a>
+            <a className="navibar" href="#">Kriptovaluta figyelő</a>
+            <a className="navibar" href="#">Rólunk</a>
+        </div>
+    )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    return (
+        <>
+            <h1 className="text-3xl bg-purple-600 text-white text-center font-Inter font-bold mx-auto top-0 left-0">React
+                App fejlesztés alatt!</h1>
+            <NavBar />
     </>
   )
 }
