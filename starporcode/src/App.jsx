8d80cf51673d1c7705e49e28d3aa984819f1c24c
JaniBT <janoscsordas072@gmail.com>
2024-02-14 07:53:05 +0100

    Update App.jsx

--- starporcode/src/App.jsx
+++ starporcode/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import './index.css'
 
+function Navbar_a( { className, href, children } ) {
+    return <a className={className} href={href}>{children}</a>
+}
+
 function NavBar() {
+    const class_navibar = "navibar"
+
     return(
         <div className="text-xl p-2 flex justify-center">
-            <a className="navibar" href="#">Kezdőlap</a>
-            <a className="navibar" href="#">Oldalak</a>
-            <a className="navibar" href="#">Tanulás</a>
-            <a className="navibar" href="#">Kriptovaluta figyelő</a>
-            <a className="navibar" href="#">Rólunk</a>
+            <Navbar_a className={class_navibar} href="index.html" children="Kezdőlap" />
+            <Navbar_a className={class_navibar} href="#" children="Oldalak" />
+            <Navbar_a className={class_navibar} href="#" children="Tanulás" />
+            <Navbar_a className={class_navibar} href="#" children="Kriptovaluta figyelő"/>
+            <Navbar_a className={class_navibar} href="#" children="Rólunk" />
         </div>
     )
 }
